27a5d9e4204ddb7b3552e8b40fc125430cefd111
Jonas Eliasson <jonas.eliasson@comhem.com>
2017-07-03 10:46:36 +0200

    Fix import

--- src/main/java/se/comhem/quantum/eventhub/EventHubReadService.java
+++ src/main/java/se/comhem/quantum/eventhub/EventHubReadService.java
@@ -11,7 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-import se.comhem.quantum.feed.twitter.PostDto;
+import se.comhem.quantum.feed.PostDto;
 
 import java.time.Duration;
 import java.util.ArrayList;
--- src/main/java/se/comhem/quantum/eventhub/EventHubWriteService.java
+++ src/main/java/se/comhem/quantum/eventhub/EventHubWriteService.java
@@ -8,7 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-import se.comhem.quantum.feed.twitter.PostDto;
+import se.comhem.quantum.feed.PostDto;
 
 import java.util.List;
 import java.util.stream.Stream;
