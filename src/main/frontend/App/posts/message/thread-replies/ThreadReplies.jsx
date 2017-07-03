1ea4ce89c9af67a1d3beb4cacacf49ad6ee272d0
Pär Strandberg <par.strandberg@comhem.com>
2017-07-03 10:47:00 +0200

    Added missing dependency stopping webpack steg to complete again. node-sass has to be rebuilt on error.

--- src/main/frontend/App/posts/message/thread-replies/ThreadReplies.jsx
+++ src/main/frontend/App/posts/message/thread-replies/ThreadReplies.jsx
@@ -5,8 +5,10 @@ export default class ThreadReplies extends PureComponent {
     render() {
         return (
             <div className="thread-replies">
+                <div className="thread-author">{this.props.post.author}</div>
                 <div className="thread__message">
-                    <i className="reply--icon"> {this.props.children} </i>{this.props.post.message}
+                    <i className="reply--icon">{this.props.children}</i>
+                    <p>{this.props.post.message}</p>
                 </div>
                 {this.props.post.replies && this.props.post.replies.map((post, j) => (
                     <ThreadReplies key={j} post={post}>{"->"}</ThreadReplies>
